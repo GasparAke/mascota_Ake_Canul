33d454583402525f7d4f2f62c59adcd1b2612b33
GasparAke <gaspar070102@gmail.com>
2021-10-17 16:08:58 -0500

    Modelos
    Se creo el modelo de especie

---
+++ app/Especie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Especie extends Model
+{
+    protected $table='especies'
+    protected $primaryKey='id_especie'
+
+    //define si la llave primaria es o no un numero autoincrementable
+    public $incrementing=true;
+
+    // activar o desactivar etiquetas de tiempo
+
+   public $timestamps=true;
+
+   public $fillable=[
+   	'id_especie',
+    'especie'
+   ];
+}
