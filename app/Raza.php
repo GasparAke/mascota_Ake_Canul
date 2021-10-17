7d209e89f188de78f730985b80c646bdd4db1ffc
GasparAke <gaspar070102@gmail.com>
2021-10-17 16:10:06 -0500

    modelo
    se creo el modelo de raza

---
+++ app/Raza.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Raza extends Model
+{
+    protected $table='razas'
+    protected $primaryKey='id_raza'
+
+    //define si la llave primaria es o no un numero autoincrementable
+    public $incrementing=true;
+
+    // activar o desactivar etiquetas de tiempo
+
+   public $timestamps=true;
+
+   public $fillable=[
+   	'id_raza',
+    'raza'
+   ];
+}
