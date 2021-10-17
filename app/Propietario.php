ca65b352335a1f1ac344eecdd6899f2d2aa3a12b
GasparAke <gaspar070102@gmail.com>
2021-10-17 16:11:34 -0500

    modelo
    se creo el modelo de propietario

---
+++ app/Propietario.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prpietario extends Model
+{
+    protected $table='propietarios'
+    protected $primaryKey='id_propietario'
+
+    //define si la llave primaria es o no un numero autoincrementable
+    public $incrementing=true;
+
+    // activar o desactivar etiquetas de tiempo
+
+   public $timestamps=true;
+
+   public $fillable=[
+   	'id_propietario',
+   	'nombre',
+   	'primer_apellido',
+   	'segundo_apellido',
+   	'genero'
+   ];
+}
